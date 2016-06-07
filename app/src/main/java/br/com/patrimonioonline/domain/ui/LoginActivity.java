4f22643b9d4e216107354b1c1b73399f148edf6e
heliofeliciano <heliofeliciano@gmail.com>
2016-06-07 07:42:57 -0300

    Login

---
+++ app/src/main/java/br/com/patrimonioonline/domain/ui/LoginActivity.java
@@ -0,0 +1,31 @@
+package br.com.patrimonioonline.domain.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import br.com.patrimonioonline.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by helio on 06/06/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_login)
+    EditText _etLogin;
+
+    @BindView(R.id.et_login_senha)
+    EditText _etSenha;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        ButterKnife.bind(this);
+    }
+}
