56bb6348f6ca2ca4e91f2761e61752bf2f18bd08
heliofeliciano <heliofeliciano@gmail.com>
2016-06-09 09:10:03 -0300

    Criando interface de Login

---
+++ app/src/main/java/br/com/patrimonioonline/domain/login/ILoginView.java
@@ -0,0 +1,13 @@
+package br.com.patrimonioonline.domain.login;
+
+/**
+ * Created by helio on 08/06/16.
+ */
+
+public interface ILoginView {
+
+    void navigateToCommit();
+    void network();
+    void loginFailed();
+
+}
--- app/src/main/java/br/com/patrimonioonline/domain/ui/LoginActivity.java
+++ app/src/main/java/br/com/patrimonioonline/domain/ui/LoginActivity.java
@@ -3,17 +3,21 @@ package br.com.patrimonioonline.domain.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 
+import br.com.patrimonioonline.BuildConfig;
 import br.com.patrimonioonline.R;
+import br.com.patrimonioonline.domain.login.ILoginView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by helio on 06/06/16.
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements ILoginView {
 
     @BindView(R.id.et_login)
     EditText _etLogin;
@@ -25,8 +29,32 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
+        setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        if (BuildConfig.DEBUG){
+            _etLogin.setText("dbseller");
+            _etSenha.setText("P@paiNoel");
+        }
+    }
+
+    @OnClick(R.id.btn_login_solicitaracesso)
+    public void btnSolicitarAcesso(View view){
+
+    }
+
+    @Override
+    public void navigateToCommit() {
+
+    }
+
+    @Override
+    public void network() {
+
+    }
+
+    @Override
+    public void loginFailed() {
+
     }
 }
