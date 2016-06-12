f646f2194c229da4e5b8cfc2a2d7d2c4f4bba466
heliofeliciano <heliofeliciano@gmail.com>
2016-06-12 11:01:05 -0300

    Implementar a interface ILoginPresenter

--- app/src/main/java/br/com/patrimonioonline/domain/login/LoginPresenter.java
+++ app/src/main/java/br/com/patrimonioonline/domain/login/LoginPresenter.java
@@ -4,7 +4,7 @@ package br.com.patrimonioonline.domain.login;
  * Created by helio on 12/06/16.
  */
 
-public class LoginPresenter {
+public class LoginPresenter implements ILoginPresenter{
 
     private ILoginView view;
     private SynchronousLoginInteractor interactor;
