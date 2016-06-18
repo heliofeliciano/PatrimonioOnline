539bd034081a9c440653e5ae096af638bce7587c
heliofeliciano <heliofeliciano@gmail.com>
2016-06-18 19:45:14 -0300

    Criando activity que listará os bens

---
+++ app/src/main/java/br/com/patrimonioonline/domain/ui/BensListaActivity.java
@@ -0,0 +1,20 @@
+package br.com.patrimonioonline.domain.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.patrimonioonline.R;
+
+/**
+ * Created by helio on 18/06/16.
+ */
+
+public class BensListaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_bens);
+    }
+}
