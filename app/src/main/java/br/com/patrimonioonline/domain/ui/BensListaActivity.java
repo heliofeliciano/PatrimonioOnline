6a4612715c8b7b41f8447d56b76946f44d72136e
heliofeliciano <heliofeliciano@gmail.com>
2016-06-20 20:31:32 -0300

    Activity cadastrar bens

--- app/src/main/java/br/com/patrimonioonline/domain/ui/BensListaActivity.java
+++ app/src/main/java/br/com/patrimonioonline/domain/ui/BensListaActivity.java
@@ -3,8 +3,11 @@ package br.com.patrimonioonline.domain.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 import br.com.patrimonioonline.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by helio on 18/06/16.
@@ -12,9 +15,14 @@ import br.com.patrimonioonline.R;
 
 public class BensListaActivity extends AppCompatActivity {
 
+    @BindView(R.id.lv_lista_bens)
+    ListView lvLista;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_bens);
+
+        ButterKnife.bind(this);
     }
 }
