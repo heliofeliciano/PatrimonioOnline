0671df6e54388cbed6139f7e1cc5f79411537095
heliofeliciano <heliofeliciano@gmail.com>
2016-07-20 21:12:46 -0300

    Inicio Dev Solicitar Placas

---
+++ app/src/main/java/br/com/patrimonioonline/domain/gcm_config/GcmPushReceiverService.java
@@ -0,0 +1,8 @@
+package br.com.patrimonioonline.domain.gcm_config;
+
+/**
+ * Created by helio on 20/07/16.
+ */
+
+public class GcmPushReceiverService {
+}
---
+++ app/src/main/java/br/com/patrimonioonline/domain/gcm_config/RegistrationIntentService.java
@@ -0,0 +1,7 @@
+package br.com.patrimonioonline.domain.gcm_config;
+
+/**
+ * Created by helio on 19/07/16.
+ */
+public class RegistrationIntentService {
+}
