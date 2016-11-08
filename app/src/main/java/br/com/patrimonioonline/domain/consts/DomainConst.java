3116c74019352194b78338cc0d3f85286ef20a04
heliofeliciano <heliofeliciano@gmail.com>
2016-11-08 15:52:34 -0300

    Commit all

--- app/src/main/java/br/com/patrimonioonline/domain/consts/DomainConst.java
+++ app/src/main/java/br/com/patrimonioonline/domain/consts/DomainConst.java
@@ -6,8 +6,11 @@ package br.com.patrimonioonline.domain.consts;
 
 public class DomainConst {
 
-    //public static final String Dominio = "10.0.100.112";
-    public static final String Dominio = "192.168.0.11";
+    public static final String Dominio = "10.51.70.114";
+    //public static final String Dominio = "192.168.0.11";
     //public static final String Dominio = "192.168.1.104";
+
+
+    //public static final String Dominio = "192.168.43.18";
 
 }
